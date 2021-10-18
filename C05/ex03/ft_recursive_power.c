83b8507ba438bdf39eb8ef82d063d9a8b478a25a
kanamycine <jinyeong5320@gmail.com>
2021-10-19 01:52:40 +0900

    after c05 perfect

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -6,20 +6,20 @@
 /*   By: jinychoi <jinychoi@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 20:55:50 by jinychoi          #+#    #+#             */
-/*   Updated: 2021/10/18 20:59:28 by jinychoi         ###   ########.fr       */
+/*   Updated: 2021/10/19 01:44:13 by jinychoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int nb, int power)
 {
-	int	result;
+	int	res;
 
-	result = nb;
+	res = nb;
 	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
 	if (power > 1)
 		return (nb * ft_recursive_power(nb, power - 1));
-	return (result);
+	return (res);
 }
