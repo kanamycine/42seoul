b4f1c62dc89ed0c3dec69f996009324c49b94c1c
kanamycine <jinyeong5320@gmail.com>
2021-11-07 17:11:24 +0900

    1

--- C08/ex00/ft.h
+++ C08/ex00/ft.h
@@ -6,7 +6,7 @@
 /*   By: jinychoi <jinychoi@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 21:18:24 by jinychoi          #+#    #+#             */
-/*   Updated: 2021/10/23 21:39:49 by jinychoi         ###   ########.fr       */
+/*   Updated: 2021/11/07 17:11:12 by jinychoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
+
 
 #endif
