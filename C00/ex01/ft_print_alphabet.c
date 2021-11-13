6f9b3cc2c67dd4a50c1474dba47abf12b562ec5a
kanamycine <jinyeong5320@gmail.com>
2021-11-13 13:54:51 +0900

    1

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: jinychoi <jinychoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 15:43:38 by jinychoi          #+#    #+#             */
-/*   Updated: 2021/11/04 21:19:43 by jinychoi         ###   ########.fr       */
+/*   Updated: 2021/11/13 13:54:44 by jinychoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,5 @@
 
 void	ft_print_alphabet(void)
 {
-
 	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
 }
