7a01071c6be1de20e3b373b36e072ab50b9e0c8b
kanamycine <jinyeong5320@gmail.com>
2021-11-29 19:12:25 +0900

    jandi

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -6,14 +6,13 @@
 /*   By: jinychoi <jinychoi@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 20:55:50 by jinychoi          #+#    #+#             */
-/*   Updated: 2021/10/19 01:44:13 by jinychoi         ###   ########.fr       */
+/*   Updated: 2021/11/29 19:12:14 by jinychoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int nb, int power)
 {
 	int	res;
-
 	res = nb;
 	if (power < 0)
 		return (0);
